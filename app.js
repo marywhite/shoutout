a6232b5f9ae09e6b0358073a70cff738cccd2c4f
maryk.december@gmail.com <maryk.december@gmail.com>
2015-04-24 10:13:25 -0500

    working scroll

---
+++ app.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+	$( window ).scroll(function() {
+		var newText = "<h2>shoutout</h2>"
+		$("#container").append(newText);
+
+
+		//$(newText).hide().appendTo("#container").fadeIn("slow");
+		//append("<h2>Some text </h2>").hide().fadeIn("slow");
+	});	
+});
+
+
+
+
